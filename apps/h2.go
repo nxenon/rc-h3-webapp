c5723dcc1005d0a6a27c1995eacf347b9c843325
M Amin Nasiri <nasiri.aminm@gmail.com>
2024-12-11 15:26:16 +0400

    handle error

--- apps/h2.go
+++ apps/h2.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/nxenon/rc-h3-webapp/routes"
 	"github.com/nxenon/rc-h3-webapp/utils"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 	"net/http"
 )
 
@@ -12,13 +14,19 @@ func StartHttp2Server(appData utils.AppData) {
 
 	mux := http.NewServeMux()
 
-	h2Server := http.Server{
-		Handler: mux,
+	h2Server := &http2.Server{}
+
+	server := &http.Server{
 		Addr:    appData.H2ListenAddr,
+		Handler: h2c.NewHandler(mux, h2Server),
 	}
 
 	routes.HandleRoutes(mux)
 
-	h2Server.ListenAndServeTLS(appData.CertPath, appData.KeyPath)
+	err := server.ListenAndServeTLS(appData.CertPath, appData.KeyPath)
+	if err != nil {
+		fmt.Printf("Failed to start server: %v\n", err)
+		panic(err)
+	}
 
 }
